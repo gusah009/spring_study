b01dfe9f92cae0802ea5d9c18b9698d5174ab329
핸모정 <gusah009@naver.com>
2021-07-08 19:10:03 +0900

    maven jar -> war, apache tomcat 추가, servlet 추가

---
+++ demo/src/main/java/servletTest/SpringdemoApplication.java
@@ -0,0 +1,29 @@
+package servletTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringdemoApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringdemoApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdemoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "world") String name) {
+		return String.format("hello %s!", name);
+	}
+
+}
