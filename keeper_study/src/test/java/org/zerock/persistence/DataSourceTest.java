4564373b7faf88629cef73edb137adeaf3ddfcb7
gram_jhm <gusah009@naver.com>
2021-09-21 17:49:18 +0900

    XML -> JAVA CONFIG, Oracle JDBC, MyBatis 적용

---
+++ keeper_study/src/test/java/org/zerock/persistence/DataSourceTest.java
@@ -0,0 +1,49 @@
+package org.zerock.persistence;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.config.RootConfig;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+import static junit.framework.TestCase.fail;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {RootConfig.class})
+@Log4j
+public class DataSourceTest {
+    @Setter(onMethod_ = { @Autowired })
+    private DataSource dataSource;
+
+    @Setter(onMethod_ = { @Autowired })
+    private SqlSessionFactory sqlSessionFactory;
+
+    @Test
+    public void testConnection() {
+        try (Connection con = dataSource.getConnection()){
+            log.info(con);
+        }catch (Exception e){
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testMyBatis() {
+        try (SqlSession session = sqlSessionFactory.openSession();
+             Connection con = session.getConnection();
+        ) {
+            log.info(session);
+            log.info(con);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+}
