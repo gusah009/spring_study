39060d82e7dfa440d29aa086b050089cfe779f69
핸모정 <gusah009@naver.com>
2022-01-14 17:53:00 +0900

    구멍가게 코딩단 프로젝트 init

---
+++ coding_dan/src/main/java/com/example/coding_dan/CodingDanApplication.java
@@ -0,0 +1,13 @@
+package com.example.coding_dan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingDanApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CodingDanApplication.class, args);
+  }
+
+}
---
+++ coding_dan/src/test/java/com/example/coding_dan/CodingDanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coding_dan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingDanApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
