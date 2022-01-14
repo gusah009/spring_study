e9458d634cd541239c093033193311ec855742c2
핸모정 <gusah009@naver.com>
2022-01-14 21:03:01 +0900

    log4jdbc-log4j2 적용 완료 ㅎㅎ

--- coding_dan/src/test/java/com/example/coding_dan/mybatis/TimeMapperTest.java
+++ coding_dan/src/test/java/com/example/coding_dan/mybatis/TimeMapperTest.java
@@ -17,16 +17,23 @@ class TimeMapperTest {
   @Test
   @DisplayName("@SELECT Annotation 테스트")
   public void testGetTimeWithAnnotation() {
-    // 분명히 TimeMapper는 인터페이스로 만들어 줬는데 알아서 적당한 객체가 들어가고 있음을 확인할 수 있습니다.
-    log.info(timeMapper.getClass().getName());
-    log.info(timeMapper.getTimeWithSelectAnnotation());
+//    // 분명히 TimeMapper는 인터페이스로 만들어 줬는데 알아서 적당한 객체가 들어가고 있음을 확인할 수 있습니다.
+//    log.info(timeMapper.getClass().getName());
+//    log.info(timeMapper.getTimeWithSelectAnnotation());
+
+    // log4jdbc-log4j2 덕분에 이제 log.info()를 따로 찍어주지 않아도 알아서 log를 남깁니다.
+    timeMapper.getTimeWithSelectAnnotation();
   }
 
   @Test
   @DisplayName("xml mapper 테스트")
   public void testGetTimeWithXML() {
-    // 분명히 TimeMapper는 인터페이스로 만들어 줬는데 알아서 적당한 객체가 들어가고 있음을 확인할 수 있습니다.
-    log.info(timeMapper.getClass().getName());
-    log.info(timeMapper.getTimeWithNoAnnotation());
+
+//    // 분명히 TimeMapper는 인터페이스로 만들어 줬는데 알아서 적당한 객체가 들어가고 있음을 확인할 수 있습니다.
+//    log.info(timeMapper.getClass().getName());
+//    log.info(timeMapper.getTimeWithNoAnnotation());
+
+    // log4jdbc-log4j2 덕분에 이제 log.info()를 따로 찍어주지 않아도 알아서 log를 남깁니다.
+    timeMapper.getTimeWithNoAnnotation();
   }
 }
