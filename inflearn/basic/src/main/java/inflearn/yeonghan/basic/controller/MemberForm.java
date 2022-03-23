ffd7b074cf1b51862e59970cb3085fa4091bc1e2
정현모 <gusah009@naver.com>
2022-03-23 20:51:17 +0900

    실제 회원 관리 웹페이지 만들어보기

---
+++ inflearn/basic/src/main/java/inflearn/yeonghan/basic/controller/MemberForm.java
@@ -0,0 +1,14 @@
+package inflearn.yeonghan.basic.controller;
+
+public class MemberForm {
+
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
