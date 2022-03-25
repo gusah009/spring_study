f91c5ce6695496abc613e39e3cde4c95191a9b47
정현모 <gusah009@naver.com>
2022-03-25 15:13:35 +0900

    basic2: init 프로젝트 생성

---
+++ inflearn/basic2/src/main/java/inflearn/yeonghan/basic2/Basic2Application.java
@@ -0,0 +1,13 @@
+package inflearn.yeonghan.basic2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Basic2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Basic2Application.class, args);
+	}
+
+}
