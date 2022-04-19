d732b5a4b59d351cfc37770e642cd94ff6da6617
정현모 <gusah009@naver.com>
2022-04-19 15:58:38 +0900

    mvc: servlet init

---
+++ inflearn/mvc_servlet/src/test/java/yeonghan/mvc_servlet/MvcServletApplicationTests.java
@@ -0,0 +1,13 @@
+package yeonghan.mvc_servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcServletApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
