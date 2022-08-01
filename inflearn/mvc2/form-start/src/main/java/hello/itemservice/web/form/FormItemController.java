f5f4dced2ce8ce9f653be2e93dc31e5a37b65ea8
정핸모 <gusah009@naver.com>
2022-08-01 10:13:58 +0900

    mvc2: 타임리프를 활용해 입력 폼 처리

--- inflearn/mvc2/form-start/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ inflearn/mvc2/form-start/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -2,60 +2,64 @@ package hello.itemservice.web.form;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/form/items")
 @RequiredArgsConstructor
 public class FormItemController {
 
-    private final ItemRepository itemRepository;
+  private final ItemRepository itemRepository;
 
-    @GetMapping
-    public String items(Model model) {
-        List<Item> items = itemRepository.findAll();
-        model.addAttribute("items", items);
-        return "form/items";
-    }
+  @GetMapping
+  public String items(Model model) {
+    List<Item> items = itemRepository.findAll();
+    model.addAttribute("items", items);
+    return "form/items";
+  }
 
-    @GetMapping("/{itemId}")
-    public String item(@PathVariable long itemId, Model model) {
-        Item item = itemRepository.findById(itemId);
-        model.addAttribute("item", item);
-        return "form/item";
-    }
+  @GetMapping("/{itemId}")
+  public String item(@PathVariable long itemId, Model model) {
+    Item item = itemRepository.findById(itemId);
+    model.addAttribute("item", item);
+    return "form/item";
+  }
 
-    @GetMapping("/add")
-    public String addForm() {
-        return "form/addForm";
-    }
+  @GetMapping("/add")
+  public String addForm(Model model) {
+    model.addAttribute("item", new Item());
+    return "form/addForm";
+  }
 
-    @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
-        Item savedItem = itemRepository.save(item);
-        redirectAttributes.addAttribute("itemId", savedItem.getId());
-        redirectAttributes.addAttribute("status", true);
-        return "redirect:/form/items/{itemId}";
-    }
+  @PostMapping("/add")
+  public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+    Item savedItem = itemRepository.save(item);
+    redirectAttributes.addAttribute("itemId", savedItem.getId());
+    redirectAttributes.addAttribute("status", true);
+    return "redirect:/form/items/{itemId}";
+  }
 
-    @GetMapping("/{itemId}/edit")
-    public String editForm(@PathVariable Long itemId, Model model) {
-        Item item = itemRepository.findById(itemId);
-        model.addAttribute("item", item);
-        return "form/editForm";
-    }
+  @GetMapping("/{itemId}/edit")
+  public String editForm(@PathVariable Long itemId, Model model) {
+    Item item = itemRepository.findById(itemId);
+    model.addAttribute("item", item);
+    return "form/editForm";
+  }
 
-    @PostMapping("/{itemId}/edit")
-    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
-        itemRepository.update(itemId, item);
-        return "redirect:/form/items/{itemId}";
-    }
+  @PostMapping("/{itemId}/edit")
+  public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+    itemRepository.update(itemId, item);
+    return "redirect:/form/items/{itemId}";
+  }
 
 }
 
